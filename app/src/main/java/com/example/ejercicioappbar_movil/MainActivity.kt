8a24edcfd63b6b2a0e97ad05a0218fcc36294fce
yubo <yubo.lin@educa.jcyl.es>
2026-01-18 17:36:57 +0100

    echo otro ejercicio

--- app/src/main/java/com/example/ejercicioappbar_movil/MainActivity.kt
+++ app/src/main/java/com/example/ejercicioappbar_movil/MainActivity.kt
@@ -5,14 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +21,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -38,10 +41,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             EjercicioAppBarMovilTheme {
                 val context = LocalContext.current
+
+                var verificarCuenta by remember { mutableStateOf(false) }
+                var mostrarDialog by remember { mutableStateOf(false) }
+
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
                         AppBarMenu(
                             title = "Mi Aplicación",
+                            verificarCuenta = verificarCuenta,
+
                             onNavigationClick = {
                                 Toast.makeText(context, "Navegación", Toast.LENGTH_SHORT).show()
                             },
@@ -50,11 +59,21 @@ class MainActivity : ComponentActivity() {
                             },
                             onMenuItemClick = { option ->
                                 Toast.makeText(context, "Menu: $option", Toast.LENGTH_SHORT).show()
-                            }
+                            },
+                            onLoginClick = { mostrarDialog = true }
                         )
                     }
 
                 ) { innerPadding ->
+                    if (mostrarDialog) {
+                        LoginDialog(
+                            onDismissRequest = { mostrarDialog = false },
+                            loginExito = {
+                                verificarCuenta = true
+                                Toast.makeText(context, "¡Login correcto!", Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    }
                     Text( text = "Cotenedo 123", modifier = Modifier.padding(innerPadding))
 
                 }
@@ -71,6 +90,8 @@ fun AppBarMenu(
     onNavigationClick: () -> Unit,
     onFavoriteClick: () -> Unit,
     onMenuItemClick: (String) -> Unit,
+    verificarCuenta: Boolean,
+    onLoginClick: () -> Unit,
     modifier: Modifier = Modifier){
     TopAppBar(
         modifier = modifier,
@@ -88,13 +109,21 @@ fun AppBarMenu(
                         contentDescription = "Favorito"
                     )
                 }
-                MenuDesplegable(onOptionSelected = onMenuItemClick)
 
+                if (verificarCuenta) {
+                    Icon(Icons.Default.AccountCircle, contentDescription = "Usuario Login")
+                }
+
+                MenuDesplegable(
+                    onOptionSelected = onMenuItemClick,
+                    onIngresarClick = onLoginClick
+                )
         })
 }
 
 @Composable
-fun MenuDesplegable(onOptionSelected: (String) -> Unit){
+fun MenuDesplegable(onOptionSelected: (String) -> Unit,
+                    onIngresarClick: () -> Unit){
     var menu_expanded by remember { mutableStateOf(false) }
 
     IconButton(onClick = { menu_expanded = true }) {
@@ -104,6 +133,15 @@ fun MenuDesplegable(onOptionSelected: (String) -> Unit){
         expanded = menu_expanded,
         onDismissRequest = { menu_expanded = false }
     ) {
+
+        DropdownMenuItem(
+            text = { Text("Ingresar") },
+            onClick = {
+                menu_expanded = false
+                onIngresarClick()
+            }
+        )
+
 
         DropdownMenuItem(
 
@@ -131,7 +169,40 @@ fun MenuDesplegable(onOptionSelected: (String) -> Unit){
     }
 }
 
+@Composable
+fun LoginDialog(
+    onDismissRequest: () -> Unit,
+    loginExito: () -> Unit
+) {
+    var usuario by remember { mutableStateOf("") }
+    var pass by remember { mutableStateOf("") }
 
+    val context = LocalContext.current
+
+    AlertDialog(
+        title = { Text("Iniciar Sesión") },
+        text = {
+            Column {
+                TextField(value = usuario, onValueChange = { usuario = it }, label = { Text("Usuario") })
+                TextField(value = pass, onValueChange = { pass = it }, label = { Text("Contraseña") })
+            }
+        },
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(onClick = {
+                if (usuario == "admin" && pass == "admin") {
+                    loginExito()
+                    onDismissRequest()
+                }else {
+                    Toast.makeText(context, "¡error de usuairo o contraseña!", Toast.LENGTH_SHORT).show()
+                }
+            }) { Text("Entrar") }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) { Text("Cancelar") }
+        }
+    )
+}
 
 
 @Preview(showBackground = true)
@@ -139,10 +210,12 @@ fun MenuDesplegable(onOptionSelected: (String) -> Unit){
 fun GreetingPreview() {
     EjercicioAppBarMovilTheme {
        AppBarMenu(
-           title = TODO(),
-           onNavigationClick = TODO(),
-           onFavoriteClick = TODO(),
-           onMenuItemClick = TODO()
+           title = "Vista Previa",
+           onNavigationClick = {},
+           onFavoriteClick = {},
+           onMenuItemClick = {},
+           verificarCuenta = true,
+           onLoginClick = {}
        )
     }
 }
